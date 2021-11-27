3c7dc9d7fb80c7737c22f431f0a816766ff398a1
Josejuanoika <josejuanoika@gmail.com>
2021-11-26 19:33:28 -0600

    tests

---
+++ DatingAppUaa.UnitTests/Tests/BuggyControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using DatingApp.Api.DTOs;
+using DatingAppUaa.UnitTests.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DatingAppUaa.UnitTests.Pruebas
+{
+    public class BuggyControllerTests
+    {
+        private string apiRoute = "api/buggy";
+        private readonly HttpClient _client;
+        private HttpResponseMessage httpResponse;
+        private string requestUri;
+        private string registeredObject;
+        private HttpContent httpContent;
+
+        public BuggyControllerTests()
+        {
+            _client = TestHelper.Instance.Client;
+        }
+
+        [Theory]
+        [InlineData("OK", "karen", "Pa$$w0rd")]
+        public async Task get_secret_ok(string statusCode, string username, string password)
+        {
+            // Arrange
+            var user = await LoginHelper.LoginUser(username, password);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", user.Token);
+            requestUri = $"{apiRoute}/auth";
+
+            // Act
+            httpResponse = await _client.GetAsync(requestUri);
+            _client.DefaultRequestHeaders.Authorization = null;
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+
+        [Theory]
+        [InlineData("NotFound")]
+        public async Task get_not_found(string statusCode)
+        {
+            // Arrange
+            
+            requestUri = $"{apiRoute}/not-fund";
+
+            // Act
+            httpResponse = await _client.GetAsync(requestUri);
+
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+
+        [Theory]
+        [InlineData("InternalServerError")]
+        public async Task get_server_error_server_error(string statusCode)
+        {
+            // Arrange
+
+            requestUri = $"{apiRoute}/server-error";
+
+            // Act
+            httpResponse = await _client.GetAsync(requestUri);
+
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+
+        [Theory]
+        [InlineData("BadRequest")]
+        public async Task get_bad_request(string statusCode)
+        {
+            // Arrange
+
+            requestUri = $"{apiRoute}/bad-request";
+
+            // Act
+            httpResponse = await _client.GetAsync(requestUri);
+
+            // Assert
+            Assert.Equal(Enum.Parse<HttpStatusCode>(statusCode, true), httpResponse.StatusCode);
+            Assert.Equal(statusCode, httpResponse.StatusCode.ToString());
+        }
+
+        #region Privated methods
+        private static string get_register_object(string roles)
+        {
+            var entityObject = new JObject()
+            {
+                { "roles", roles }
+            };
+            return entityObject.ToString();
+        }
+        private StringContent get_http_content(string objectToEncode)
+        {
+            return new StringContent(objectToEncode, Encoding.UTF8, "application/json");
+        }
+
+        #endregion
+    }
+}
